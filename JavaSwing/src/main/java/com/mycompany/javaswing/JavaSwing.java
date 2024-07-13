669a8787f74285babfc53fc9450d755aefc96a1a
mrKuncay008 <kuncaymuhammad@gmail.com>
2024-07-14 01:14:57 +0700

    Update 14 junly

---
+++ JavaSwing/src/main/java/com/mycompany/javaswing/JavaSwing.java
@@ -0,0 +1,12 @@
+package com.mycompany.javaswing;
+
+/**
+ *
+ * @author kunca
+ */
+public class JavaSwing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
